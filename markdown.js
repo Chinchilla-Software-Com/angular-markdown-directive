1bbbb76dea23baae8e2722eb596dcf5c2d79e17a
Grover Jackson <grover@cdmdotnet.com>
2020-09-23 13:25:44 +1200

    Added support for markdown extensions to return other angularjs components.

--- markdown.js
+++ markdown.js
@@ -6,8 +6,8 @@
 
 'use strict';
 
-angular.module('btford.markdown', ['ngSanitize']).
-	provider('markdownConverter', function () {
+angular.module('btford.markdown', ['ngSanitize'])
+	.provider('markdownConverter', function () {
 		var opts = {};
 		return {
 			config: function (newOpts) {
@@ -17,26 +17,58 @@ angular.module('btford.markdown', ['ngSanitize']).
 				return new showdown.Converter(opts);
 			}
 		};
-	}).
-	directive('btfMarkdown', ['$sanitize', 'markdownConverter', function ($sanitize, markdownConverter) {
+	})
+	.directive('btfMarkdown', ['$sanitize', "$compile", 'markdownConverter', function ($sanitize, $compile, markdownConverter) {
+		var func = function ($scope, newVal, oldValue) {
+			if (newVal === oldValue)
+				return false;
+			var html = "";
+			if (newVal) {
+				var rawHtml = markdownConverter.makeHtml(newVal);
+				if ($scope.onPreSanitize)
+					rawHtml = $scope.onPreSanitize()(rawHtml);
+				html = $sanitize(rawHtml)
+				if ($scope.onPostSanitize)
+					html = $scope.onPostSanitize()(html);
+			}
+			$scope.processedHtml = html;
+			return true;
+		};
+
 		return {
 			restrict: 'AE',
+			scope: {
+				onPreSanitize: "&?",
+				onPostSanitize: "&?",
+				ngMarkdown: "=?",
+				btfMarkdown: "=?"
+			},
 			link: function (scope, element, attrs) {
-				if (attrs.ngMarkdown) {
-					scope.$watch(attrs.ngMarkdown, function (newVal) {
-						var html = newVal ? $sanitize(markdownConverter.makeHtml(newVal)) : '';
-						element.html(html);
-					});
-				}
-				else if (attrs.btfMarkdown) {
-					scope.$watch(attrs.btfMarkdown, function (newVal) {
-						var html = newVal ? $sanitize(markdownConverter.makeHtml(newVal)) : '';
-						element.html(html);
-					});
-				} else {
-					var html = $sanitize(markdownConverter.makeHtml(element.text()));
-					element.html(html);
-				}
-			}
+				scope.$watch("processedHtml", function (newVal, oldValue) {
+					if (newVal === oldValue)
+						return;
+					var e = $compile(scope.processedHtml)(scope);
+					element.empty();
+					element.append(e);
+				});
+
+				var e = $compile(scope.processedHtml)(scope);
+				element.empty();
+				element.append(e);
+			},
+			controller: ["$scope", function ($scope) {
+				if ($scope.ngMarkdown)
+					func($scope, $scope.ngMarkdown);
+				else if ($scope.btfMarkdown)
+					func($scope, $scope.btfMarkdown);
+
+				$scope.$watch("ngMarkdown", function (newVal, oldValue) {
+					func($scope, newVal, oldValue);
+				});
+
+				$scope.$watch("btfMarkdown", function (newVal, oldValue) {
+					func($scope, newVal, oldValue);
+				});
+			}]
 		};
 	}]);
